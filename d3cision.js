0f6db71344bbe3fbd1d5eea6ff9cbf86fe41cfdd
Kunst Fuentes <jbkunst@gmail.com>
2016-08-12 17:45:04 -0400

    starting hover effects

--- d3cision.js
+++ d3cision.js
@@ -1,26 +1,26 @@
 function d3cision() {
-  
+
   // defaults (don't forget: have functions)
   var textnodesize = "14px";
   var debug = false;
   var shape = 0.75;
   var sep = 7.5;
+  var primarycolor = "#ccc";
+  var secondarycolor  = "transparent" // "#f0f0f0";
+  var accentcolor = "#303F9F";
+  var textcolor = "transparent" // "#212121";
+  var transitiontime = 500;
 
   // internals
-  var primarycolor = "#ccc";
-  var secondarycolor  = "#f0f0f0";
-  var accentcolor = "#303F9F";
-  var textcolor = "#212121";
-  
   var textposition = {x : 0, y : 0};
   var margin = { top: 25, right: 25, bottom: 25, left: 25 };
   var rectpars = { width: 8 };
   var colors = ["red", "blue"];
 
   function chart(selection) {
-    
+
     selection.each(function(data, i) {
-      
+
       var width = this.offsetWidth - margin.left - margin.right;
       var height = this.offsetHeight - margin.top - margin.bottom;
       var d3cisionid = makeid();
@@ -35,25 +35,26 @@ function d3cision() {
       var svg = d3.select(this).append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom);
-        
+
       var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      
+
       // declares a tree layout and assigns the size
       var treemap = d3.tree()
         .size([width, height]);
-      
+
       //  assigns the data to a hierarchy using parent-child relationships
       var nodes = d3.hierarchy(data);
-      
+
       // maps the node data to the tree layout
       nodes = treemap(nodes);
-      
+
       // adds the links between the nodes
       var links =  g.selectAll(".link2")
         .data(nodes.descendants().slice(1))
         .enter();
-        
+
+      // the external (the real ones)
       var links1 = links
         .append("path")
         .attr("d3cisionid", d3cisionid)
@@ -63,14 +64,12 @@ function d3cision() {
         .attr("stroke-width", "2px")
         .attr("nodeid", function(d){ return d.data.name; })
         .attr("d", function(d) {
-          
           var yd = (1 - shape) * d.y + shape * d.parent.y;
           var curve = getshape(d.x, d.y, d.parent.x, d.parent.y, yd);
-          
           return curve;
-          
         });
-        
+
+      // the internal, which add some fancy style IMHO
       var link2 = links
         .append("path")
         .attr("d3cisionid", d3cisionid)
@@ -80,20 +79,15 @@ function d3cision() {
         .attr("stroke-width", "2px")
         .attr("nodeid", function(d){ return d.data.name; })
         .attr("d", function(d) {
-          
-          sep2 = sep * ((d.data.side == "left") ? 1 : -1);
-          
-          var yd = (1 - shape) * d.y + shape * d.parent.y,
+          var sep2 = sep * ((d.data.side == "left") ? 1 : -1),
+            yd = (1 - shape) * d.y + shape * d.parent.y,
             m = (d.parent.y - yd) / (d.parent.x - d.x),
             bp = sep * Math.sqrt(m * m + 1) + d.parent.y,
             ydp = m * (d.x + sep2) - (m * d.parent.x - bp);
-          
           var curve = getshape(d.x + sep2, d.y - sep, d.parent.x, bp, ydp);
-          
           return curve;
-          
         });
-      
+
       // adds each node as a group
       var node = g.selectAll(".node")
           .data(nodes.descendants())
@@ -105,7 +99,7 @@ function d3cision() {
           .attr("transform", function(d) {
             return "translate(" + d.x + "," + d.y + ")";
           });
-      
+
       // adds the rects to the node
       /*
       var rects = node.append("rect")
@@ -122,7 +116,7 @@ function d3cision() {
         .attr("width", rectpars.width)
         .attr("height", 10);
       */
-      
+
       // adds the text to the node
       var texts = node.append("text")
         .attr("d3cisionid", d3cisionid)
@@ -131,21 +125,20 @@ function d3cision() {
         .attr("font-size", textnodesize)
         .attr("fill", textcolor)
         .attr("dy", ".35em")
-        //.attr("y", function(d) { return d.children ? -20 : 20; })
-        .attr("y", -20)
+        .attr("y", -20) //.attr("y", function(d) { return d.children ? -20 : 20; })
         .attr("x", function(d){
           var textanchor = (d.data.side == "left") ? "end" : "left";
           var x = 5 * ((textanchor == "end") ? -1 : 1);
           return x;
         })
-        //.style("text-anchor", "middle")
         .style("text-anchor", function(d){
+          //.style("text-anchor", "middle")
           var textanchor = (d.data.side == "left") ? "end" : "left";
           return textanchor;
         })
         .text(function(d) { return d.data.rule + " (" + d.data.name + ")"; });
-        
-      // text rule 
+
+      // text rule
       var textrule = g.selectAll(".text")
         .attr("d3cisionid", d3cisionid)
         .data([textposition]).enter()
@@ -153,47 +146,97 @@ function d3cision() {
         .attr("x", function(d) { return d.x; })
         .attr("y", function(d) { return d.y; })
         .text("");
-        
-        
+
       function mouseover(d) {
-        console.log(d.data.name);
-        /*d3.selectAll("[d3cisionid='ynppv']").selectAll("[nodeid='4']");*/
+
+        d3.selectAll("path[d3cisionid='" + d3cisionid + "'][nodeid='" + d.data.name + "']")
+          .transition().duration(transitiontime)
+          .attr("stroke", accentcolor);
+
+        d3.selectAll("text[d3cisionid='" + d3cisionid + "'][nodeid='" + d.data.name + "']")
+            .transition().duration(transitiontime)
+            .attr("fill", accentcolor);
       }
-      
-      //links.on('mouseover', mouseover);
-      //rects.on('mouseover', mouseover);
-      
+
+      function mouseout(d) {
+        d3.selectAll(".d3cisionid-link-external[d3cisionid='" + d3cisionid + "'][nodeid='" + d.data.name + "']")
+          .transition().duration(transitiontime)
+          .attr("stroke", secondarycolor);
+
+        d3.selectAll(".d3cisionid-link-internal[d3cisionid='" + d3cisionid + "'][nodeid='" + d.data.name + "']")
+          .transition().duration(transitiontime)
+          .attr("stroke", primarycolor);
+
+        d3.selectAll("text[d3cisionid='" + d3cisionid + "'][nodeid='" + d.data.name + "']")
+          .transition().duration(transitiontime)
+          .attr("fill", secondarycolor);
+      }
+
+      d3.selectAll("[d3cisionid='" + d3cisionid + "']").on('mouseover', mouseover);
+      d3.selectAll("[d3cisionid='" + d3cisionid + "']").on('mouseout', mouseout);
+      // link2.on('mouseout', mouseout);
+      // rects.on('mouseover', mouseover);
 
     });
-    
+
   }
 
   chart.textnodesize = function(_) {
     if (!arguments.length) return textnodesize;
     textnodesize = _;
-    return chart;    
+    return chart;
   };
-  
+
   chart.shape = function(_) {
     if (!arguments.length) return shape;
     shape = _;
-    return chart;    
+    return chart;
   };
-  
+
   chart.sep = function(_) {
     if (!arguments.length) return sep;
     sep = _;
-    return chart;    
-  };  
-  
+    return chart;
+  };
+
+  chart.primarycolor = function(_) {
+    if (!arguments.length) return primarycolor;
+    primarycolor = _;
+    return chart;
+  };
+
+  chart.secondarycolor = function(_) {
+    if (!arguments.length) return secondarycolor;
+    secondarycolor = _;
+    return chart;
+  };
+
+  chart.accentcolor = function(_) {
+    if (!arguments.length) return accentcolor;
+    accentcolor = _;
+    return chart;
+  };
+
+  chart.textcolor = function(_) {
+    if (!arguments.length) return textcolor;
+    textcolor = _;
+    return chart;
+  };
+
+  chart.transitiontime = function(_) {
+    if (!arguments.length) return transitiontime;
+    transitiontime = _;
+    return chart;
+  };
+
   chart.debug = function(_) {
     if (!arguments.length) return debug;
     debug = _;
-    return chart;    
-  };  
-  
+    return chart;
+  };
+
   return chart;
-  
+
 }
 
 // Auxiliar functions
@@ -203,12 +246,11 @@ function makeid() {
 }
 
 function getshape(x, y, xp, yp, yd) {
-  
+
    curve =  "M" + x + "," + y
      + " " + x  + "," + yd
      + " " + xp + "," + yp;
-     
-    return curve;
-  
-}
 
+    return curve;
+
+}
